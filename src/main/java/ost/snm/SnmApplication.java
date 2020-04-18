196eb119b39bdc9681e5e3321b3870a4d1f66ddb
Tomer Blecher <tomer.blecher.brolix@gmail.com>
2020-04-18 15:19:41 +0300

    Initial Commit.

---
+++ src/main/java/ost/snm/SnmApplication.java
@@ -0,0 +1,13 @@
+package ost.snm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ost/snm/SnmApplicationTests.java
@@ -0,0 +1,13 @@
+package ost.snm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
